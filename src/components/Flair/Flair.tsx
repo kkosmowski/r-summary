56717b7e29c0f507de35556c29582212ffb2ac55
Krzysztof Kosmowski <krzysztof.kosmowski@tsh.io>
2025-01-21 10:58:19 +0100

    feat: themes finished

--- src/components/Flair/Flair.tsx
+++ src/components/Flair/Flair.tsx
@@ -2,14 +2,19 @@ import { PostItem } from '~/types/reddit';
 
 import styles from './Flair.module.scss';
 
-const getTextColor = (color: 'light' | 'dark') => {
+const getTextColor = (color: 'light' | 'dark' | null) => {
+  if (!color) return 'var(--color-fg-50)';
   return color === 'dark' ? '#000' : '#fff';
+};
+const getBackgroundColor = (color: string | null) => {
+  return color ?? 'var(--color-bg-200)';
 };
 
 export const Flair = ({ flair }: Pick<PostItem, 'flair'>) => {
-  const { text, color, backgroundColor } = flair;
+  const { text, color, backgroundColor: bgColor } = flair;
 
   const textColor = getTextColor(color);
+  const backgroundColor = getBackgroundColor(bgColor);
 
   return (
     <div className={styles.badge} style={{ color: textColor, backgroundColor }}>
